bb4df6bdee90bf17eaef4b93c6103994a586d150
kvaluckas <kostas.valuckas@knf.stud.vu.lt>
2023-10-02 23:09:00 +0300

    Teksto pakeitimo mygtukas

--- app/src/main/java/com/example/valuckas_lab1/MainActivity.java
+++ app/src/main/java/com/example/valuckas_lab1/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.valuckas_lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+
+    }
+
+    public void onBtnChangeTextClick(View view) {
+        this.tvMain.setText("Labas Pasauli");
     }
 }
